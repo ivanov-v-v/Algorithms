d33d637e673a30bde0b35eb4eabc9ff049a05e39
Viacheslav Ivanov <vy.vl.ivanov@gmail.com>
2016-07-14 14:41:35 +0300

    Generic segment tree in C++11

---
+++ data-structures/templated_segment_tree.cpp
@@ -0,0 +1,58 @@
+template<typename T>
+class segment_tree {
+private:
+    const int MAXN;
+    const function<T(T, T)> op;
+    const T id;
+    vector<T> t;
+
+    void _build_tree(const vector<T>& a, int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr)>>1;
+            _build_tree(a, v*2, tl, tm);
+            _build_tree(a, v*2+1, tm+1, tr);
+            t[v] = op(t[v*2], t[v*2+1]);
+        }
+    }
+
+    void _update_position(int v, int tl, int tr, int pos, const T& val) {
+        if (tl == tr) {
+            t[v] = val;
+        } else {
+            int tm = (tl + tr)>>1;
+            if (pos <= tm)  _update_position(v*2, tl, tm, pos, val);
+            else            _update_position(v*2+1, tm+1, tr, pos, val);
+            t[v] = op(t[v*2], t[v*2+1]);
+        }
+    }
+
+    T _fold_range(int v, int tl, int tr, int l, int r) const {
+        if (l > r) return id;
+        if (l == tl && tr == r) {
+            return t[v];
+        } else {
+            int tm = (tl + tr)>>1;
+            return op(_fold_range(v*2, tl, tm, l, min(tm, r)),
+                        _fold_range(v*2+1, tm+1, tr, max(l, tm+1), r));
+        }
+    }
+
+public:
+    segment_tree(const int SIZE, const function<T(T, T)> bin_op, T op_id) {
+        MAXN = SIZE;
+        id = op_id;
+        op = bin_op;
+        t.resize(4*SIZE+1);
+    }
+    inline void build_tree(const vector<T>& a) {
+        _build_tree(a, 1, 0, MAXN-1);
+    }
+    inline void update_position(int pos, const T& val) {
+        _update_position(1, 0, MAXN-1, pos, val);
+    }
+    inline T fold_range(int l, int r) const {
+        return _fold_range(1, 0, MAXN-1, l, r);
+    }
+};
