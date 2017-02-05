648b8688af74b64b9d81413a1c4215d838ab919d
Viacheslav Ivanov <vy.vl.ivanov@gmail.com>
2017-02-05 13:35:39 +0300

    Update binarySearchTree.cpp

--- data-structures/binarySearchTree.cpp
+++ data-structures/binarySearchTree.cpp
@@ -81,17 +81,15 @@ public:
             } else if (!root->right) {
                 Node* leftChild = root->left;
                 delete root;
-                root = leftChild;
-                return root;
+                return root = leftChild;
             } else if (!root->left) {
                 Node* rightChild = root->right;
                 delete root;
-                root = rightChild;
-                return root;
+                return root = rightChild;
             } else {
                 Node* newRoot = findMinimum(root->right);
                 root->value = newRoot->value;
-                erase(root->right, newRoot->value);
+                root->right = erase(root->right, newRoot->value);
                 return root;
             }
         }
@@ -132,22 +130,6 @@ public:
         T minVal = findMinimum(root)->value;
         root = erase(root, minVal);
         return minVal;
-    }
-    T predecessor(const T& key) const {
-        Node *entry = find(root, key);
-        if (!entry) {
-            return key;
-        }
-        Node *prev = findMaximum(entry->left);
-        return prev ? prev->value : key;
-    }
-    T successor(const T& key) const {
-        Node* entry = find(root, key);
-        if (!entry) {
-            return key;
-        }
-        Node *next = findMinimum(entry->right);
-        return next ? next->value : key;
     }
     void inorderTraversal() const {
         inorderTraversal(root);
